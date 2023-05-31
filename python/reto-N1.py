abcfb3f7a49ff4c2d2dac2ddab39248732c974df
ardev-21 <ariel2001linares@gmail.com>
2023-05-30 23:16:39 -0300

    reto n1 en python

--- python/reto-N1.py
+++ python/reto-N1.py
@@ -6,3 +6,13 @@
  * - Múltiplos de 5 por la palabra "buzz".
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
 """
+
+for i in range(1, 101):
+    if (i % 3 == 0) and (i % 5 == 0):
+        print("fizzbuzz")
+    elif (i % 5 == 0):
+        print("buzz")
+    elif (i % 3 == 0):
+        print("fizz")
+    else:
+        print(i)
